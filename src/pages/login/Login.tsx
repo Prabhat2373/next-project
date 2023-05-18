307acb0fa4c4d9a97cc34f4ef2ced2b041f3866e
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2023-05-18 14:59:30 +0530

    Update Login.tsx

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -100,7 +100,10 @@ export default function Login() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled={loading}
+                className={`flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
+                  loading ? "bg-gray-600 hover:bg-gray-600" : ""
+                }`}
               >
                 {loading ? "Loading.." : "Sign in"}
               </button>
